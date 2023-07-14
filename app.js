4059223055984705b9470c7291cd65109cc70758
shivee-yadav <shiveeyadav089@gmail.com>
2023-07-14 23:20:54 +0530

    validation error

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
 const express = require('express');
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const expressValidator = require('express-validator');
+const { validationResult } = require('express-validator');
 require('dotenv').config();
 
-const userRoutes=require('./routes/user');
+const userRoutes = require('./routes/user');
 
-const app= express();
+const app = express();
 
 mongoose.connect(process.env.DATABASE, {
-    useNewUrlParser:true,
-    family: 4
+  useNewUrlParser: true,
+  family: 4
 }).then(() => console.log('DB Connected'));
 
-
-//middleware
+// Middleware
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(expressValidator());
 
-//routes middleware
-app.use('/api',userRoutes);
+// Routes Middleware
+app.use('/api', userRoutes);
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
-app.listen(port, ()=> {
-    console.log(`Server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
--- validator/index.js
+++ validator/index.js
@@ -1,13 +1,23 @@
-exports.userSignupValidator= (req,res) => {
-    req.check('name','Name is requires').notEmpty()
-    req.check('email', 'email must be between 3 to 32 characters').matches(/.+\@.+\..+/)
-    .withMessage("Email must contain @")
-    .isLength({
-        min: 4,
-        max: 32
-    });
-    req.check('password', 'Password is required').notEmpty()
-    req.check('password').isLength({min: 6}).withMessage('Password must contain at least 6 characters')
-    .matches(/\d/).withMessage('Password must contain a number');
-    
+const { check, validationErrors } = require("express-validator");
+
+exports.userSignupValidator =(res,req,next) => { 
+  check("name", "Name is required").notEmpty();
+  check("email")
+    .isEmail().withMessage("Invalid email")
+    .isLength({ min: 3, max: 32 }).withMessage("Email must be between 3 and 32 characters");
+  check("password", "Password is required").notEmpty();
+  check("password")
+    .isLength({ min: 6 }).withMessage("Password must contain at least 6 characters")
+    .matches(/\d/).withMessage("Password must contain a number");
+
+    const errors = validationErrors(req);
+    if(errors) {
+        const firstError = errors.map(error => error.msg)[0];
+        return res.status(400).json({error : firstError});
+    }
+
+    next();
+
+
+
 };
